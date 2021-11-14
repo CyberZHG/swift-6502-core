8e3c20ea53ca5d6b91af5471569c6f3b5897e2a8
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2021-11-14 12:42:13 +0800

    Add ORA

--- CPU6502Core/Tests/CPU6502CoreTests/Logical/OpORATests.swift
+++ CPU6502Core/Tests/CPU6502CoreTests/Logical/OpORATests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import CPU6502Core
 
-final class OpANDTests: XCTestCase {
+final class OpORATests: XCTestCase {
     
     var cpu = CPU6502()
     var memory = Memory()
@@ -11,19 +11,19 @@ final class OpANDTests: XCTestCase {
         self.memory.reset()
     }
     
-    func testANDImmediate() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x29, 0b00001010])
+    func testORAImmediate() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x09, 0b00001010])
         self.cpu.PC = 0x0000
-        self.cpu.A = 0b11110010
+        self.cpu.A = 0b01110010
         var actualCycle = try self.cpu.execute(memory, maxCycle: 2)
         XCTAssertEqual(actualCycle, 2)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0b00000010)
+        XCTAssertEqual(self.cpu.A, 0b01111010)
         XCTAssertEqual(self.cpu.P, 0b00100100)
         
-        self.memory.setBytes(start: 0x0000, bytes: [0x29, 0b00001010])
+        self.memory.setBytes(start: 0x0000, bytes: [0x09, 0b00000000])
         self.cpu.PC = 0x0000
-        self.cpu.A = 0b11110000
+        self.cpu.A = 0b00000000
         actualCycle = try self.cpu.execute(memory, maxCycle: 2)
         XCTAssertEqual(actualCycle, 2)
         XCTAssertEqual(self.cpu.PC, 0x0002)
@@ -31,30 +31,30 @@ final class OpANDTests: XCTestCase {
         XCTAssertEqual(self.cpu.P, 0b00100110)
         XCTAssertEqual(self.cpu.Z, true)
         
-        self.memory.setBytes(start: 0x0000, bytes: [0x29, 0xFF])
+        self.memory.setBytes(start: 0x0000, bytes: [0x09, 0xFF])
         self.cpu.PC = 0x0000
         self.cpu.A = 0b11110000
         actualCycle = try self.cpu.execute(memory, maxCycle: 2)
         XCTAssertEqual(actualCycle, 2)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0b11110000)
+        XCTAssertEqual(self.cpu.A, 0xFF)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         XCTAssertEqual(self.cpu.N, true)
     }
     
-    func testANDZeroPage() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x25, 0x00])
+    func testORAZeroPage() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x05, 0x00])
         self.cpu.PC = 0x0000
         self.cpu.A = 0xAA
         let actualCycle = try self.cpu.execute(memory, maxCycle: 3)
         XCTAssertEqual(actualCycle, 3)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0x20)
-        XCTAssertEqual(self.cpu.P, 0b00100100)
+        XCTAssertEqual(self.cpu.A, 0xAF)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDZeroPageX() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x35, 0x10])
+    func testORAZeroPageX() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x15, 0x10])
         self.memory.setBytes(start: 0x0010, bytes: [0xAB, 0xCD])
         self.cpu.PC = 0x0000
         self.cpu.A = 0xAA
@@ -62,7 +62,7 @@ final class OpANDTests: XCTestCase {
         var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0xAA)
+        XCTAssertEqual(self.cpu.A, 0xAB)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
@@ -70,12 +70,12 @@ final class OpANDTests: XCTestCase {
         actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0x88)
+        XCTAssertEqual(self.cpu.A, 0xEF)
         XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDAbsolute() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x3D, 0xCD, 0xAB])
+    func testORAAbsolute() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x0D, 0xCD, 0xAB])
         self.memory.setBytes(start: 0xABCD, bytes: [0xFE])
         self.cpu.PC = 0x0000
         self.cpu.A = 0xAA
@@ -83,12 +83,12 @@ final class OpANDTests: XCTestCase {
         let actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xAA)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDAbsoluteX() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x3D, 0xCD, 0xAB])
+    func testORAAbsoluteX() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x1D, 0xCD, 0xAB])
         self.memory.setBytes(start: 0xABCD, bytes: [0xFE, 0xFC])
         self.memory.setBytes(start: 0xAC2D, bytes: [0xFA])
         self.cpu.PC = 0x0000
@@ -97,7 +97,7 @@ final class OpANDTests: XCTestCase {
         var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xAA)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
@@ -105,7 +105,7 @@ final class OpANDTests: XCTestCase {
         actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
@@ -113,12 +113,12 @@ final class OpANDTests: XCTestCase {
         actualCycle = try self.cpu.execute(memory, maxCycle: 5)
         XCTAssertEqual(actualCycle, 5)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDAbsoluteY() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x39, 0xCD, 0xAB])
+    func testORAAbsoluteY() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x19, 0xCD, 0xAB])
         self.memory.setBytes(start: 0xABCD, bytes: [0xFE, 0xFC])
         self.memory.setBytes(start: 0xAC2D, bytes: [0xFA])
         self.cpu.PC = 0x0000
@@ -127,7 +127,7 @@ final class OpANDTests: XCTestCase {
         var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xAA)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
@@ -135,7 +135,7 @@ final class OpANDTests: XCTestCase {
         actualCycle = try self.cpu.execute(memory, maxCycle: 4)
         XCTAssertEqual(actualCycle, 4)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
@@ -143,12 +143,12 @@ final class OpANDTests: XCTestCase {
         actualCycle = try self.cpu.execute(memory, maxCycle: 5)
         XCTAssertEqual(actualCycle, 5)
         XCTAssertEqual(self.cpu.PC, 0x0003)
-        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.A, 0xFE)
         XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDIndexedIndirect() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x21, 0x40])
+    func testORAIndexedIndirect() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x01, 0x40])
         self.memory.setBytes(start: 0x0041, bytes: [0xCD, 0xAB])
         self.memory.setBytes(start: 0xABCD, bytes: [0x42])
         self.cpu.PC = 0x0000
@@ -157,12 +157,12 @@ final class OpANDTests: XCTestCase {
         let actualCycle = try self.cpu.execute(memory, maxCycle: 6)
         XCTAssertEqual(actualCycle, 6)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0x02)
-        XCTAssertEqual(self.cpu.P, 0b00100100)
+        XCTAssertEqual(self.cpu.A, 0xEA)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
-    func testANDIndirectIndexed() throws {
-        self.memory.setBytes(start: 0x0000, bytes: [0x31, 0x40])
+    func testORAIndirectIndexed() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x11, 0x40])
         self.memory.setBytes(start: 0x0040, bytes: [0xCD, 0xAB])
         self.memory.setBytes(start: 0xABCD, bytes: [0x42, 0x43])
         self.memory.setBytes(start: 0xACBD, bytes: [0x44])
@@ -172,16 +172,16 @@ final class OpANDTests: XCTestCase {
         var actualCycle = try self.cpu.execute(memory, maxCycle: 5)
         XCTAssertEqual(actualCycle, 5)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0x02)
-        XCTAssertEqual(self.cpu.P, 0b00100100)
+        XCTAssertEqual(self.cpu.A, 0xEB)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
         
         self.cpu.PC = 0x0000
         self.cpu.Y = 0xF0
         actualCycle = try self.cpu.execute(memory, maxCycle: 6)
         XCTAssertEqual(actualCycle, 6)
         XCTAssertEqual(self.cpu.PC, 0x0002)
-        XCTAssertEqual(self.cpu.A, 0x00)
-        XCTAssertEqual(self.cpu.P, 0b00100110)
+        XCTAssertEqual(self.cpu.A, 0xEF)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
     }
     
 }
