e94b4b46bcd8e9e2ccae5a2e8677cb48b244b296
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2021-11-14 12:48:50 +0800

    Add EOR

---
+++ CPU6502Core/Tests/CPU6502CoreTests/Logical/OpEORTests.swift
@@ -0,0 +1,187 @@
+import XCTest
+@testable import CPU6502Core
+
+final class OpEORTests: XCTestCase {
+    
+    var cpu = CPU6502()
+    var memory = Memory()
+    
+    override func setUp() {
+        self.cpu.reset()
+        self.memory.reset()
+    }
+    
+    func testEORImmediate() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x49, 0b00001010])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0b01110010
+        var actualCycle = try self.cpu.execute(memory, maxCycle: 2)
+        XCTAssertEqual(actualCycle, 2)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0b01111000)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+        
+        self.memory.setBytes(start: 0x0000, bytes: [0x49, 0b00000000])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0b00000000
+        actualCycle = try self.cpu.execute(memory, maxCycle: 2)
+        XCTAssertEqual(actualCycle, 2)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0x00)
+        XCTAssertEqual(self.cpu.P, 0b00100110)
+        XCTAssertEqual(self.cpu.Z, true)
+        
+        self.memory.setBytes(start: 0x0000, bytes: [0x49, 0xFF])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0b01110000
+        actualCycle = try self.cpu.execute(memory, maxCycle: 2)
+        XCTAssertEqual(actualCycle, 2)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0b10001111)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+        XCTAssertEqual(self.cpu.N, true)
+    }
+    
+    func testEORZeroPage() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x45, 0x00])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        let actualCycle = try self.cpu.execute(memory, maxCycle: 3)
+        XCTAssertEqual(actualCycle, 3)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0xEF)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+    }
+    
+    func testEORZeroPageX() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x55, 0x10])
+        self.memory.setBytes(start: 0x0010, bytes: [0xAB, 0xCD])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.X = 0
+        var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0x01)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.X = 1
+        actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0xCC)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+    }
+    
+    func testEORAbsolute() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x4D, 0xCD, 0xAB])
+        self.memory.setBytes(start: 0xABCD, bytes: [0xFE])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.X = 0
+        let actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0x54)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+    }
+    
+    func testEORAbsoluteX() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x5D, 0xCD, 0xAB])
+        self.memory.setBytes(start: 0xABCD, bytes: [0xFE, 0xFC])
+        self.memory.setBytes(start: 0xAC2D, bytes: [0xFA])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.X = 0
+        var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0x54)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.X = 1
+        actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.X = 0x60
+        actualCycle = try self.cpu.execute(memory, maxCycle: 5)
+        XCTAssertEqual(actualCycle, 5)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0x52)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+    }
+    
+    func testEORAbsoluteY() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x59, 0xCD, 0xAB])
+        self.memory.setBytes(start: 0xABCD, bytes: [0xFE, 0xFC])
+        self.memory.setBytes(start: 0xAC2D, bytes: [0xFA])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.Y = 0
+        var actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0x54)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.Y = 1
+        actualCycle = try self.cpu.execute(memory, maxCycle: 4)
+        XCTAssertEqual(actualCycle, 4)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0xA8)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.Y = 0x60
+        actualCycle = try self.cpu.execute(memory, maxCycle: 5)
+        XCTAssertEqual(actualCycle, 5)
+        XCTAssertEqual(self.cpu.PC, 0x0003)
+        XCTAssertEqual(self.cpu.A, 0x52)
+        XCTAssertEqual(self.cpu.P, 0b00100100)
+    }
+    
+    func testEORIndexedIndirect() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x41, 0x40])
+        self.memory.setBytes(start: 0x0041, bytes: [0xCD, 0xAB])
+        self.memory.setBytes(start: 0xABCD, bytes: [0x42])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.X = 1
+        let actualCycle = try self.cpu.execute(memory, maxCycle: 6)
+        XCTAssertEqual(actualCycle, 6)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0xE8)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+    }
+    
+    func testEORIndirectIndexed() throws {
+        self.memory.setBytes(start: 0x0000, bytes: [0x51, 0x40])
+        self.memory.setBytes(start: 0x0040, bytes: [0xCD, 0xAB])
+        self.memory.setBytes(start: 0xABCD, bytes: [0x42, 0x43])
+        self.memory.setBytes(start: 0xACBD, bytes: [0x44])
+        self.cpu.PC = 0x0000
+        self.cpu.A = 0xAA
+        self.cpu.Y = 1
+        var actualCycle = try self.cpu.execute(memory, maxCycle: 5)
+        XCTAssertEqual(actualCycle, 5)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0xE9)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+        
+        self.cpu.PC = 0x0000
+        self.cpu.Y = 0xF0
+        actualCycle = try self.cpu.execute(memory, maxCycle: 6)
+        XCTAssertEqual(actualCycle, 6)
+        XCTAssertEqual(self.cpu.PC, 0x0002)
+        XCTAssertEqual(self.cpu.A, 0xAD)
+        XCTAssertEqual(self.cpu.P, 0b10100100)
+    }
+    
+}
